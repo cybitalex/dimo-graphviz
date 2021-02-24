b440e9668d44e47b9e0409b1a1f29eb915148c4d
Hersh Patel <hersh@DIMO.zone>
2021-02-23 19:20:34 -0500

    Binded Ctrl-z for undo.

--- src/index.js
+++ src/index.js
@@ -69,6 +69,7 @@ let layout = {
 let expandArray = [];
 let collapseArray = [];
 let shiftKeydown = false;
+let ctrlKeydown = false;
 let CANVAS_WIDTH = 800,
   CANVAS_HEIGHT = 800;
 
@@ -330,155 +331,6 @@ G6.registerNode(
   "single-node"
 );
 
-// Custom real node
-G6.registerNode(
-  "real-node",
-  {
-    draw(cfg, group) {
-      let r = 100;
-      if (isNumber(cfg.size)) {
-        r = cfg.size / 2;
-      } else if (isArray(cfg.size)) {
-        r = cfg.size[0] / 2;
-      }
-      const style = cfg.style || {};
-      const colorSet = cfg.colorSet || colorSets[0];
-
-      // halo for hover
-      group.addShape("circle", {
-        attrs: {
-          x: 0,
-          y: 0,
-          r: r + 5,
-          fill: style.fill || colorSet.mainFill || "#2B384E",
-          opacity: 0.9,
-          lineWidth: 0
-        },
-        name: "halo-shape",
-        visible: false
-      });
-
-      // focus stroke for hover
-      group.addShape("circle", {
-        attrs: {
-          x: 0,
-          y: 0,
-          r: r + 5,
-          fill: style.fill || colorSet.mainFill || "#2B384E",
-          stroke: "#fff",
-          strokeOpacity: 0.85,
-          lineWidth: 1
-        },
-        name: "stroke-shape",
-        visible: false
-      });
-
-      const keyShape = group.addShape("circle", {
-        attrs: {
-          ...style,
-          x: 0,
-          y: 0,
-          r,
-          fill: colorSet.mainFill,
-          stroke: colorSet.mainStroke,
-          lineWidth: 2,
-          cursor: "pointer"
-        },
-        name: "aggregated-node-keyShape"
-      });
-
-      let labelStyle = {};
-      if (cfg.labelCfg) {
-        labelStyle = Object.assign(labelStyle, cfg.labelCfg.style);
-      }
-
-      if (cfg.label) {
-        const text = cfg.label;
-        let labelStyle = {};
-        let refY = 0;
-        if (cfg.labelCfg) {
-          labelStyle = Object.assign(labelStyle, cfg.labelCfg.style);
-          refY += cfg.labelCfg.refY || 0;
-        }
-        let offsetY = 0;
-        const fontSize = labelStyle.fontSize < 8 ? 8 : labelStyle.fontSize;
-        const lineNum = cfg.labelLineNum || 1;
-        offsetY = lineNum * (fontSize || 12);
-        group.addShape("text", {
-          attrs: {
-            text,
-            x: 0,
-            y: r + refY + offsetY + 5,
-            textAlign: "center",
-            textBaseLine: "alphabetic",
-            cursor: "pointer",
-            fontSize,
-            fill: "#fff",
-            opacity: .85,
-            fontWeight: 400,
-            stroke: global.edge.labelCfg.style.stroke
-          },
-          name: "text-shape",
-          className: "text-shape"
-        });
-      }
-
-      // tag for new node
-      if (cfg.new) {
-        group.addShape("circle", {
-          attrs: {
-            x: r - 3,
-            y: -r + 3,
-            r: 4,
-            fill: "#6DD400",
-            lineWidth: 0.5,
-            stroke: "#FFFFFF"
-          },
-          name: "typeNode-tag-circle"
-        });
-      }
-
-      return keyShape;
-    },
-    setState: (name, value, item) => {
-      const group = item.get("group");
-      if (name === "layoutEnd" && value) {
-        const labelShape = group.find((e) => e.get("name") === "text-shape");
-        if (labelShape) labelShape.set("visible", true);
-      } else if (name === "hover") {
-        if (item.hasState("focus")) {
-          return;
-        }
-        const halo = group.find((e) => e.get("name") === "halo-shape");
-        const keyShape = item.getKeyShape();
-        const colorSet = item.getModel().colorSet || colorSets[0];
-        if (value) {
-          halo && halo.show();
-          keyShape.attr("fill", colorSet.activeFill);
-        } else {
-          halo && halo.hide();
-          keyShape.attr("fill", colorSet.mainFill);
-        }
-      } else if (name === "focus") {
-        const stroke = group.find((e) => e.get("name") === "stroke-shape");
-        const label = group.find((e) => e.get("name") === "text-shape");
-        const keyShape = item.getKeyShape();
-        const colorSet = item.getModel().colorSet || colorSets[0];
-        if (value) {
-          stroke && stroke.show();
-          keyShape.attr("fill", colorSet.selectedFill);
-          label && label.attr("fontWeight", 800);
-        } else {
-          stroke && stroke.hide();
-          keyShape.attr("fill", colorSet.mainFill); // '#2B384E'
-          label && label.attr("fontWeight", 400);
-        }
-      }
-    },
-    update: undefined
-  },
-  "dimo-node"
-);
 
 // Custom the quadratic edge for multiple edges between one node pair
 G6.registerEdge(
@@ -585,106 +437,7 @@ G6.registerEdge(
 );
 
 // Custom the line edge for single edge between one node pair
-G6.registerEdge(
-  "custom-line",
-  {
-    setState: (name, value, item) => {
-      const group = item.get("group");
-      const model = item.getModel();
-      if (name === "focus") {
-        const keyShape = group.find((ele) => ele.get("name") === "edge-shape");
-        const back = group.find((ele) => ele.get("name") === "back-line");
-        if (back) {
-          back.stopAnimate();
-          back.remove();
-          back.destroy();
-        }
-        const arrow = model.style.endArrow;
-        if (value) {
-          if (keyShape.cfg.animation) {
-            keyShape.stopAnimate(true);
-          }
-          keyShape.attr({
-            strokeOpacity: animateOpacity,
-            opacity: animateOpacity,
-            stroke: "#fff",
-            endArrow: {
-              ...arrow,
-              stroke: "#fff",
-              fill: "#fff"
-            }
-          });
-          if (model.isReal) {
-            const { path, stroke, lineWidth } = keyShape.attr();
-            const back = group.addShape("path", {
-              attrs: {
-                path,
-                stroke,
-                lineWidth,
-                opacity: animateBackOpacity
-              },
-              name: "back-line"
-            });
-            back.toBack();
-            const length = keyShape.getTotalLength();
-            keyShape.animate(
-              (ratio) => {
-                // the operations in each frame. Ratio ranges from 0 to 1 indicating the prograss of the animation. Returns the modified configurations
-                const startLen = ratio * length;
-                // Calculate the lineDash
-                const cfg = {
-                  lineDash: [startLen, length - startLen]
-                };
-                return cfg;
-              },
-              {
-                repeat: true, // Whether executes the animation repeatly
-                duration // the duration for executing once
-              }
-            );
-          } else {
-            const lineDash = keyShape.attr("lineDash");
-            const totalLength = lineDash[0] + lineDash[1];
-            let index = 0;
-            keyShape.animate(
-              () => {
-                index++;
-                if (index > totalLength) {
-                  index = 0;
-                }
-                const res = {
-                  lineDash,
-                  lineDashOffset: -index
-                };
-                // returns the modified configurations here, lineDash and lineDashOffset here
-                return res;
-              },
-              {
-                repeat: true, // whether executes the animation repeatly
-                duration // the duration for executing once
-              }
-            );
-          }
-        } else {
-          keyShape.stopAnimate();
-          const stroke = "#acaeaf";
-          const opacity = model.isReal ? realEdgeOpacity : virtualEdgeOpacity;
-          keyShape.attr({
-            stroke,
-            strokeOpacity: opacity,
-            opacity: opacity,
-            endArrow: {
-              ...arrow,
-              stroke,
-              fill: stroke
-            }
-          });
-        }
-      }
-    }
-  },
-  "single-edge"
-);
+
 
 const descendCompare = (p) => {
   // 这是比较函数
@@ -1415,14 +1168,31 @@ const stopLayout = () => {
 const bindListener = (graph) => {
   graph.on("keydown", (evt) => {
     const code = evt.key;
+    console.log("keydown",code)
     if (!code) {
       return;
     }
+    
+    if (code.toLowerCase() == 'z') {
+      if (ctrlKeydown) {
+        self.graphToolbar.undo();
+      }
+    }
+
     if (code.toLowerCase() === "shift") {
       shiftKeydown = true;
     } else {
       shiftKeydown = false;
     }
+    if (code.toLowerCase() === "control" || code.toLowerCase() === "meta") {
+      ctrlKeydown = true;
+    } else {
+      ctrlKeydown = false;
+    }
+
+
+
+
   });
   graph.on("keyup", (evt) => {
     const code = evt.key;
@@ -1432,6 +1202,10 @@ const bindListener = (graph) => {
     if (code.toLowerCase() === "shift") {
       shiftKeydown = false;
     }
+    if (code.toLowerCase() === "control" || code.toLowerCase() === "meta") {
+      ctrlKeydown = false;
+    }
+
   });
   graph.on("node:mouseenter", (evt) => {
     const { item } = evt;
@@ -4160,7 +3934,7 @@ function initGraph(nodes, edges_) {
 
 
     const toolbar = new G6.ToolBar()
-
+    self.graphToolbar = toolbar;
 
 
     graph = new G6.Graph({
