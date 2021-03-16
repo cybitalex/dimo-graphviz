ebf708564f3c81339f41bd484b75fccb7bcc92f9
Hersh Patel <hersh@DIMO.zone>
2021-03-16 04:48:21 -0400

    Added search.

--- src/index.js
+++ src/index.js
@@ -2,33 +2,14 @@ import G6 from "@antv/g6";
 import insertCss from "insert-css";
 import { isNumber, isArray } from "@antv/util";
 import { GraphQLClient, gql, request } from 'graphql-request'
+import './dataTable.js'
+import './modal.js'
 const Lrequest = require('request')
 // import React from "react";
 // import ReactDOM from "react-dom";
 
+self.modalOpen = false;
 
-
-var settingsModal = document.getElementById("settingsModal");
-
-// Get the <span> element that closes the modal
-var settingsModalCloseButton = document.getElementById("settingsModalCloseButton");
-
-// When the user clicks on the button, open the modal 
-const settingsModalOpen = function() {
-  settingsModal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-settingsModalCloseButton.onclick = function() {
-  settingsModal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == settingsModal) {
-    settingsModal.style.display = "none";
-  }
-}
 
 
 
@@ -1243,6 +1224,11 @@ const stopLayout = () => {
 
 const bindListener = (graph) => {
   graph.on("keydown", (evt) => {
+    console.log("modal",self.modalOpen)
+    if(self.modalOpen) {
+      return
+    }
+
     const code = evt.key;
     console.log("keydown",code)
     if (!code) {
@@ -1311,6 +1297,11 @@ function wait(ms){
 
 
   graph.on("keyup", (evt) => {
+    console.log("modal",self.modalOpen)
+    if(self.modalOpen) {
+      return
+    }
+
     const code = evt.key;
     if (!code) {
       return;
@@ -4315,10 +4306,7 @@ function initGraph(nodes, edges_, useLayout=true) {
             
             break;
           case "organizeAll":
-            //layout.instance.stop();
-
-
-
+           
             var radLayoutConfig = {
                 type: 'radial',
                 center: [item._cfg.model.x,item._cfg.model.y], // The center of the graph by default
@@ -4334,10 +4322,6 @@ function initGraph(nodes, edges_, useLayout=true) {
 
             }
             graph.updateLayout(radLayoutConfig)
-            // layout.instance = new G6.Layout["radial"](radLayoutConfig);
-            // layout.instance.init({nodes:graph.getNodes(),edges:graph.getEdges()});
-            // layout.instance.execute();
-            //graph.refresh();
 
 
             break;
@@ -4433,6 +4417,10 @@ function initGraph(nodes, edges_, useLayout=true) {
         <svg class="icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="25" height="24" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" viewBox="0 0 28 28"><g fill="none"><path d="M14 9.5a4.5 4.5 0 1 0 0 9a4.5 4.5 0 0 0 0-9zM11 14a3 3 0 1 1 6 0a3 3 0 0 1-6 0z" fill="#000000"/><path d="M21.71 22.395l-1.728-.759a1.72 1.72 0 0 0-1.542.086c-.467.27-.765.747-.824 1.284l-.208 1.88a.923.923 0 0 1-.703.796a11.67 11.67 0 0 1-5.412 0a.923.923 0 0 1-.702-.796l-.208-1.877a1.701 1.701 0 0 0-.838-1.281a1.694 1.694 0 0 0-1.526-.086l-1.728.759a.92.92 0 0 1-1.043-.215a12.064 12.064 0 0 1-2.707-4.672a.924.924 0 0 1 .334-1.016l1.527-1.128a1.7 1.7 0 0 0 0-2.74l-1.527-1.125a.924.924 0 0 1-.334-1.017A12.059 12.059 0 0 1 5.25 5.821a.92.92 0 0 1 1.043-.214l1.72.757a1.707 1.707 0 0 0 2.371-1.376l.21-1.878a.923.923 0 0 1 .715-.799c.881-.196 1.78-.3 2.704-.311c.902.01 1.8.115 2.68.311a.922.922 0 0 1 .715.8l.209 1.878a1.701 1.701 0 0 0 1.688 1.518c.233 0 .464-.049.68-.144l1.72-.757a.92.92 0 0 1 1.043.214a12.057 12.057 0 0 1 2.708 4.667a.924.924 0 0 1-.333 1.016l-1.525 1.127c-.435.32-.698.829-.698 1.37c0 .54.263 1.049.699 1.37l1.526 1.126c.316.234.45.642.334 1.017a12.065 12.065 0 0 1-2.707 4.667a.92.92 0 0 1-1.043.215zm-5.447-.198a3.162 3.162 0 0 1 1.425-1.773a3.22 3.22 0 0 1 2.896-.161l1.344.59a10.565 10.565 0 0 0 1.97-3.398l-1.189-.877v-.001a3.207 3.207 0 0 1-1.309-2.578c0-1.027.497-1.98 1.307-2.576l.002-.001l1.187-.877a10.56 10.56 0 0 0-1.971-3.397l-1.333.586l-.002.001c-.406.18-.843.272-1.286.272a3.202 3.202 0 0 1-3.178-2.852v-.002l-.163-1.46a11.476 11.476 0 0 0-1.95-.193c-.674.009-1.33.074-1.975.193l-.163 1.461A3.207 3.207 0 0 1 7.41 7.737l-1.336-.588a10.558 10.558 0 0 0-1.971 3.397l1.19.877a3.201 3.201 0 0 1 0 5.155l-1.19.878a10.565 10.565 0 0 0 1.97 3.403l1.345-.59a3.194 3.194 0 0 1 2.878.16a3.2 3.2 0 0 1 1.579 2.411v.005l.162 1.464c1.297.255 2.63.255 3.927 0l.162-1.467c.024-.22.07-.437.138-.645z" fill="#000000"/></g>
         </svg>
     </li>
+    <li code="search">
+       <svg class="icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="20" height="24" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" viewBox="0 0 24 24"><g fill="#000000"><path d="M15.25 0a8.25 8.25 0 0 0-6.18 13.72L1 22.88l1.12 1l8.05-9.12A8.251 8.251 0 1 0 15.25.01V0zm0 15a6.75 6.75 0 1 1 0-13.5a6.75 6.75 0 0 1 0 13.5z"/></g></svg>
+    </li>
+
 
 </ul>`
         },
@@ -4456,7 +4444,9 @@ function initGraph(nodes, edges_, useLayout=true) {
                   const pageUrl = getStateURL();
                   
             } else if (code == "settings") {
-                    settingsModalOpen()
+                    self.settingsModalOpen()
+            } else if (code == "search") {
+                    self.searchModalOpen()
             }
 
         }
